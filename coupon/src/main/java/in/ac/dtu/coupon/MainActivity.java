c420b8d40faba88fe9f493b25b055c24fd90b719
Umair Khan <omerjerk@gmail.com>
2013-12-26 15:35:02 +0530

    class to read from the stored json file
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- coupon/src/main/java/in/ac/dtu/coupon/MainActivity.java
+++ coupon/src/main/java/in/ac/dtu/coupon/MainActivity.java
@@ -1,22 +1,30 @@
 package in.ac.dtu.coupon;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new ReadFromJson().execute();
     }
 
 
@@ -33,13 +41,51 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        switch(item.getGroupId()) {
+        switch(item.getItemId()) {
             case R.id.action_settings :
                 return true;
             case R.id.action_refresh :
-                new UpdateCoupons(getApplicationContext(), false).execute();
+                new UpdateCoupons(MainActivity.this, false).execute();
+                return  true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ReadFromJson extends AsyncTask<Void, Void, ArrayList<JSONObject>> {
+
+        @Override
+        protected ArrayList<JSONObject> doInBackground(Void... v) {
+
+            String jsonString = "";
+
+            try {
+                //Log.d(TAG, getActivity().getFilesDir() + "data.json");
+                File cacheFile = new File(getApplicationContext().getFilesDir(), "data.json");
+
+                BufferedReader br = new BufferedReader(new FileReader(cacheFile));
+                jsonString = br.readLine();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                new UpdateCoupons(MainActivity.this, true).execute();
+            }
+
+            ArrayList<JSONObject> couponList = new ArrayList<JSONObject>();
+
+            try {
+                JSONArray jsonArray = new JSONArray(jsonString);
+
+                for (int i = 1; i < jsonArray.length(); ++i) {
+                    JSONObject obj = jsonArray.getJSONObject(i);
+                    couponList.add(obj);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return couponList;
+        }
     }
 
 }
