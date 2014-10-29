7c5afffa925131550c65572ce2219687a67b768e
Umair Khan <omerjerk@gmail.com>
2014-10-29 13:26:55 +0530

    Rewrite the card adapter
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- coupon/src/main/java/in/ac/dtu/coupon/CustomCardAdapter.java
+++ coupon/src/main/java/in/ac/dtu/coupon/CustomCardAdapter.java
@@ -5,15 +5,14 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.afollestad.cardsui.Card;
-import com.afollestad.cardsui.CardAdapter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -25,26 +24,26 @@ import in.tosc.coupon.R;
 /**
  * Created by omerjerk on 26/12/13.
  */
-public class CustomCardAdapter extends CardAdapter<Card> {
+public class CustomCardAdapter extends RecyclerView.Adapter<CustomCardAdapter.ViewHolder> {
 
     private ArrayList<JSONObject> couponList;
     private Context context;
+    private LayoutInflater mInflater;
 
-    public CustomCardAdapter(Context context, ArrayList<JSONObject> couponList) {
-        super(context, R.layout.card_layout); // the custom card layout is passed to the super constructor instead of every individual card
+    public CustomCardAdapter(ArrayList<JSONObject> couponList, Context context) {
         this.couponList = couponList;
         this.context = context;
-        setAccentColor(Color.parseColor("#ff0099cc"));
+        mInflater = LayoutInflater.from(context);
     }
 
-    public Drawable getVendorIcon (String vendorName) {
+    public Drawable getVendorIcon(String vendorName) {
         vendorName = vendorName.toLowerCase();
         int vendorIconId = context.getResources().getIdentifier(vendorName, "drawable", "in.tosc.coupon");
         Drawable vendorIcon = context.getResources().getDrawable(vendorIconId);
         return vendorIcon;
     }
 
-    public boolean haveVendorIcon (String vendorName) {
+    public boolean haveVendorIcon(String vendorName) {
         String[] vendorArray = context.getResources().getStringArray(R.array.vendors);
         for (int i = 0; i < vendorArray.length; i++) {
             if (vendorArray[i].equalsIgnoreCase(vendorName)) {
@@ -54,90 +53,83 @@ public class CustomCardAdapter extends CardAdapter<Card> {
         return false;
     }
 
+    // Create new views (invoked by the layout manager)
     @Override
-    protected boolean onProcessThumbnail(ImageView icon, Card card) {
-        // Optional, you can modify properties of the icon ImageView here.
-        return super.onProcessThumbnail(icon, card);
+    public CustomCardAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                           int viewType) {
+        // create a new view
+        CardView view = (CardView) LayoutInflater.from(context)
+                .inflate(R.layout.view_card, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
+    // Replace the contents of a view (invoked by the layout manager)
     @Override
-    protected boolean onProcessTitle(TextView title, Card card, int accentColor) {
-        // Optional, you can modify properties of the title textview here.
-        return super.onProcessTitle(title, card, accentColor);
-    }
-
-    @Override
-    protected boolean onProcessContent(TextView content, Card card) {
-        // Optional, you can modify properties of the content textview here.
-        content.setTextColor(Color.DKGRAY);
-        return super.onProcessContent(content, card);
-
-    }
-
-    @Override
-    protected boolean onProcessMenu(View view, final Card card) {
-        // Sets up a card's menu (custom_card.xml makes this a star)
-        return super.onProcessMenu(view, card);
-    }
-
-    @Override
-    public View onViewCreated(final int index, View recycled, Card item) {
-
-        TextView couponName = (TextView) recycled.findViewById(R.id.name_coupon);
-        LinearLayout cardLayout = (LinearLayout) recycled.findViewById(R.id.card_layout);
-        ImageView vendorIcon = (ImageView) recycled.findViewById(R.id.vendor_icon);
-
-        if (recycled == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            recycled = inflater.inflate(R.layout.card_layout, null);
-        }
-        try{
-            if(couponName != null) {
-                String vendorName = couponList.get(index - 1).getString("name");
-                couponName.setText("WEBSITE : " + vendorName);
-                couponName.setTextColor(Color.parseColor("#444444"));
-                cardLayout.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast toast = Toast.makeText(context,
-                                "Long press this card to go to website to redeem coupon",
-                                Toast.LENGTH_SHORT);
-                        toast.show();
-                    }
-                });
-                cardLayout.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View view) {
-                        Uri uri = null;
-                        try {
-                            uri = Uri.parse(couponList.get(index - 1).getString("url"));
-                        } catch (JSONException e) {
-                            uri = Uri.parse("http://google.com");
-                            e.printStackTrace();
-                        }
-                        Intent webIntent = new Intent(Intent.ACTION_VIEW, uri);
-                        webIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        context.startActivity(webIntent);
-                        return false;
-                    }
-                });
-
-
-                if (haveVendorIcon(vendorName)) {
-                    vendorIcon.setImageDrawable(getVendorIcon(vendorName));
-                } else {
-                    vendorIcon.setImageDrawable(getVendorIcon("none"));
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        try {
+            String vendorName = couponList.get(position).getString("name");
+            holder.couponName.setText("WEBSITE : " + vendorName);
+            holder.couponName.setTextColor(Color.parseColor("#444444"));
+            holder.root.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast toast = Toast.makeText(context,
+                            "Long press this card to go to website to redeem coupon",
+                            Toast.LENGTH_SHORT);
+                    toast.show();
                 }
+            });
+            holder.root.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    Uri uri = null;
+                    try {
+                        uri = Uri.parse(couponList.get(position).getString("url"));
+                    } catch (JSONException e) {
+                        uri = Uri.parse("http://google.com");
+                        e.printStackTrace();
+                    }
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW, uri);
+                    webIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(webIntent);
+                    return false;
+                }
+            });
+
+
+            if (haveVendorIcon(vendorName)) {
+                holder.vendorIcon.setImageDrawable(getVendorIcon(vendorName));
+            } else {
+                holder.vendorIcon.setImageDrawable(getVendorIcon("none"));
             }
-
-
-
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
 
-        // Optional, you can modify properties of other views that you add to the card layout that aren't the icon, title, content...
-        return super.onViewCreated(index, recycled, item);
+    @Override
+    public int getItemCount() {
+        return couponList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView couponName;
+        public TextView couponDescription;
+        public ImageView vendorIcon;
+        public CardView root;
+
+        public ViewHolder(CardView view) {
+            super(view);
+            root = view;
+            couponName = (TextView) view.findViewById(R.id.coupon_name);
+            couponDescription = (TextView) view.findViewById(R.id.coupon_description);
+            vendorIcon = (ImageView) view.findViewById(R.id.vendor_icon);
+        }
     }
 
 }
